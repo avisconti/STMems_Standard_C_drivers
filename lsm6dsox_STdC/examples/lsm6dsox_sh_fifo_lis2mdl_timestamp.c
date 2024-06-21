637a5b6bf9f0d219b02d20030760792556c63400
Armando Visconti <armando.visconti@st.com>
2024-06-21 13:05:18 +0200

    Add platform_delay() prototype is few examples
    Few examples are not building properly as the platform_delay()
    prototype was missing.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- lsm6dsox_STdC/examples/lsm6dsox_sh_fifo_lis2mdl_timestamp.c
+++ lsm6dsox_STdC/examples/lsm6dsox_sh_fifo_lis2mdl_timestamp.c
@@ -143,7 +143,6 @@ static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len);
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
-static void platform_delay(uint32_t ms);
 static void tx_com( uint8_t *tx_buffer, uint16_t len );
 static void platform_delay(uint32_t ms);
 static void platform_init(void);
